4ae573c803843a944cbcd799476a40b0d65aa343
Alexey Velikiy <gmpota@gmail.com>
2012-04-01 18:18:31 +0400

    Positions testing

--- tests/Tests.java
+++ tests/Tests.java
@@ -1,7 +1,9 @@
 package drinker.tests;
 
+import drinker.ObjectEventHandler;
 import drinker.World;
-import drinker.WorldObjects.Toper;
+import drinker.WorldObject;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,24 +13,48 @@ import java.io.PrintStream;
 
 public class Tests {
 
-    private World world;    
-
+    private World world;
+    
+    
     @Before
     public void InitWorld() {
         PrintStream printStream = new PrintStream(new OutputStream() {
             @Override
             public void write(int i) throws IOException {
-                
+
             }
         });
+
         world = new World(16, 16, printStream);
+       
     }
 
+    private boolean pos03visited;
     @Test
-    public void TestS() {
-        world.addObject(new Toper(1,1));
-        System.out.println("asd");
-
+    public void TestPositions(){        
+        for (int i = 0; i < 16; i++) {
+            for (int j = 0; j < 16; j++) {
+                final WorldObject ground = world.getObjectAtXY(i, j).iterator().next();
+                ground.addEnterHandler(new ObjectEventHandler() {
+                    public void onEvent(WorldObject o) {
+                        TestPositions(ground, o);
+                    }
+                });
+            }
+        }
+        for (int i = 1; i < 1000; i++) {
+            world.tick();
+        }
+        Assert.assertTrue("Position (0,3) wasn't visited.", pos03visited);
+    }
+    
+    
+    public void TestPositions(WorldObject ground, WorldObject o) {
+        boolean badPos = ground.getX() == 16 && ground.getY() == 0;
+        Assert.assertFalse("Illegal position (16, 0)", badPos);
+        if(ground.getX() == 0 && ground.getY() == 3){
+            pos03visited = true;
+        }
     }
 
 }
